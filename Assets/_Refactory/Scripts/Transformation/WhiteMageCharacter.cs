5edb3150f2c14207cd9f7b3db1293b409d3b0178
Federico <f.bachis91@gmail.com>
2026-09-08 22:10:11 +0200

    White Mage Base implementation

--- Assets/_Refactory/Scripts/Transformation/WhiteMageCharacter.cs
+++ Assets/_Refactory/Scripts/Transformation/WhiteMageCharacter.cs
@@ -11,59 +11,59 @@ namespace CharacterSystem
             {
                 stats.LoseMana(spell.cost);
                 animator.SetTrigger(spell.spellName);
-                throw new System.NotImplementedException();
+                status.TriggerImmunity();
             }
 
             return false;
         }
         public override void ApplyDark(PotionScriptable ps)
         {
-            throw new System.NotImplementedException();
+            status.TriggerImmunity();
         }
 
         public override void ApplyFire(PotionScriptable ps)
         {
-            throw new System.NotImplementedException();
+            status.TriggerImmunity();
         }
 
         public override void ApplyIce(PotionScriptable ps)
         {
-            throw new System.NotImplementedException();
+            status.TriggerImmunity();
         }
 
         public override void ApplyGrass(PotionScriptable ps)
         {
-            throw new System.NotImplementedException();
+            status.TriggerImmunity();
         }
 
         public override void ApplyGround(PotionScriptable ps)
         {
-            throw new System.NotImplementedException();
+            status.TriggerImmunity();
         }
 
         public override void ApplyHeal(PotionScriptable ps)
         {
-            throw new System.NotImplementedException();
+            status.TriggerImmunity();
         }
 
         public override void ApplyLava(PotionScriptable ps)
         {
-            throw new System.NotImplementedException();
+            status.TriggerImmunity();
         }
 
         public override void ApplyLight(PotionScriptable ps)
         {
-            throw new System.NotImplementedException();
+            status.TriggerImmunity();
         }
 
         public override void ApplyPoison(PotionScriptable ps)
         {
-            throw new System.NotImplementedException();
+            status.TriggerImmunity();
         }
 
         public override void ApplyWet(PotionScriptable ps)
         {
-            throw new System.NotImplementedException();
+            status.TriggerImmunity();
         }
 
 
@@ -74,30 +74,24 @@ namespace CharacterSystem
 
         public override void PoisonTick()
         {
-            throw new System.NotImplementedException();
         }
         public override void FireTick()
         {
-            throw new System.NotImplementedException();
         }
         public override void GroundTick()
         {
-            throw new System.NotImplementedException();
         }
 
         public override void IceTick()
         {
-            throw new System.NotImplementedException();
         }
 
         public override void OnEnterTransformation()
         {
-            throw new System.NotImplementedException();
         }
 
         public override void OnExitTransformation()
         {
-            throw new System.NotImplementedException();
         }
 
     }
